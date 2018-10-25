22ffefa2e778987f838171369ff54eb16103f795
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2018-10-25 14:59:27 +0000

    Update Verkefni4.py

--- Verkefni4.py
+++ Verkefni4.py
@@ -40,7 +40,4 @@ def static(skra):
 def villa(error):
     return "<h2 style ='color:red> þessi síða fannst ekki</h2> "
 
-try:
-    bottle.run(host="0.0.0.0", port=os.environ.get('PORT'))
-except:
-    run(debug=True)
+run(host="0.0.0.0", port=os.environ.get('PORT'))
